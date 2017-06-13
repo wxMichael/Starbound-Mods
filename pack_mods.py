96dc5d94e9b114122f0f4c7a02390e3b328cb4f7
wxMichael <wxMichael@users.noreply.github.com>
2017-06-13 09:16:04 -0400

    Created Python script to pack all mods.

---
+++ pack_mods.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+if not os.path.isdir('_packed'):
+	os.mkdir('_packed')
+
+for item in os.scandir():
+	if item.is_dir() and not item.name.startswith(('.', '_')):
+		subprocess.call([r'C:\Program Files (x86)\Steam\steamapps\common\Starbound\win32\asset_packer.exe', item.name, '_packed\\' + item.name + '.pak'])
+
+
+os.system('pause')
